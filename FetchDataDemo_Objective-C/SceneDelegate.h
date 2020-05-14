6534f9dddbb208837b91fa09c1d501b65295e984
Neal Xiao <nealxiao@Neal-MacBook-Pro.local>
2020-05-14 19:04:03 +0800

    git init. Added README.md and .gitignore

---
+++ FetchDataDemo_Objective-C/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  FetchDataDemo_Objective-C
+//
+//  Created by Neal Xiao on 2020/5/14.
+//  Copyright © 2020 NealXiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ FetchDataDemo_Objective-C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FetchDataDemo_Objective-C
+//
+//  Created by Neal Xiao on 2020/5/14.
+//  Copyright © 2020 NealXiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
